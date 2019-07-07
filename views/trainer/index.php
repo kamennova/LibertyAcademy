8a6a90e9b17379a2a98170ba2097e63edc3c7ca3
Masha <mashushvik.2001@gmail.com>
2019-07-07 19:59:33 +0300

    Layout bug fixed

--- views/trainer/index.php
+++ views/trainer/index.php
@@ -19,7 +19,6 @@ use yii\widgets\LinkPager;
 
 $this->title = 'Teachers | Liberty Academy';
 
-$this->registerCssFile('/css/trainer_index.css');
 $this->registerCssFile('/css/list_layout.css');
 
 $trainers = $provider->getModels();
@@ -61,7 +60,6 @@ $serviceListSubQuery = (new \yii\db\Query())
 
 $serviceList = Service::find()
     ->select('service_name')
-    ->where(['in', 'id', $serviceListSubQuery])
     ->indexBy('id')
     ->orderBy('id')
     ->column();
@@ -73,7 +71,8 @@ $langListSubQuery = (new \yii\db\Query())
     ->from('trainer_language')
     ->distinct();
 
-$langList = Language::find()->select('lang_name')->where(['in', 'id', $langListSubQuery])->indexBy('id')->orderBy('id')->column();
+$langList = Language::find()->select('lang_name')->where(['in', 'id', $langListSubQuery])
+    ->indexBy('id')->orderBy('id')->column();
 
 ?>
 
@@ -87,79 +86,84 @@ $langList = Language::find()->select('lang_name')->where(['in', 'id', $langListS
 
                 $smallThumbsList = '';
 
-                foreach($smallThumbs as $smallThumb){
+                foreach ($smallThumbs as $smallThumb) {
                     $smallThumbsList .= "<li class='small-thumb'><img src='{$smallThumb}'/></li>";
                 }
 
-                if($trainersNumber != 0 && $trainersNumber < 3 && $smallThumbsNumber < $trainersNumber){
-                    for ($i = 0; $i < $trainersNumber; $i++){
+                if ($trainersNumber != 0 && $trainersNumber < 3 && $smallThumbsNumber < $trainersNumber) {
+                    for ($i = 0; $i < $trainersNumber; $i++) {
                         $smallThumbsList .= "<li class='small-thumb'></li>";
                     }
                 };
 
-                echo ($smallThumbsList == '') ? null : '<ul class="small-thumbs-list trainers-thumbs-list">' . $smallThumbsList . '</ul>';
+                if ($smallThumbsList != '') {
+                    echo '<ul class="small-thumbs-list trainers-thumbs-list">' . $smallThumbsList . '</ul>';
+                }
 
             } ?>
-            <span class="number trainers-number"><?= $trainersNumber . ' teacher' . (($trainersNumber !== 1) ? 's' : null) ?></span>
+
+            <span class="number trainers-number">
+                <?= $trainersNumber ?> teacher <?= (($trainersNumber !== 1) ? 's' : null) ?>
+            </span>
         </div>
 
         <h1 class="filter-title">Find a skilled teacher</h1>
 
         <div class="form-wrapper">
-        <?php $form = ActiveForm::begin([
-            'layout' => 'default',
-            'action' => Url::to(['trainer/index']),
-            'method' => 'GET',
-            'options' => ['class' => 'filter-form']
-        ]); ?>
+            <?php $form = ActiveForm::begin([
+                'layout' => 'default',
+                'action' => Url::to(['trainer/index']),
+                'method' => 'GET',
+                'options' => ['class' => 'filter-form']
+            ]); ?>
 
-        <div class="fields-container">
+            <div class="fields-container">
 
-            <?= $form->field($condition, 'name', ['horizontalCssClasses' => ['wrapper' => false, 'offset' => false]],
-                ['options' => ['class' => 'sort-select-field-2']])->widget(Select2::className(), [
-                'data' => $nameList,
-                'options' => [
-                    'placeholder' => 'Name',
-                    'onchange' => 'this.form.submit()',
-                    'class' => 'form-control',
-                ],
-                'pluginOptions' => [
-                    'allowClear' => true,
-                    'margin' => '0 8px'
+                <?= $form->field($condition, 'name', ['horizontalCssClasses' => ['wrapper' => false, 'offset' => false]],
+                    ['options' => ['class' => 'sort-select-field-2']])->widget(Select2::class, [
+                    'data' => $nameList,
+                    'options' => [
+                        'placeholder' => 'Name',
+                        'onchange' => 'this.form.submit()',
+                        'class' => 'form-control',
+                    ],
+                    'pluginOptions' => [
+                        'allowClear' => true,
+                        'margin' => '0 8px'
 
-                ]])->label(false) ?>
+                    ]])->label(false) ?>
 
-            <?= $form->field($condition, 'teachcountry_id')
-                ->dropDownList($countryList, [
-                    'prompt' => 'Teaching in...',
-                    'onchange' => 'this.form.submit()'
-                ])->label(false); ?>
+                <?= $form->field($condition, 'teachcountry_id')
+                    ->dropDownList($countryList, [
+                        'prompt' => 'Teaching in...',
+                        'onchange' => 'this.form.submit()'
+                    ])->label(false); ?>
 
-            <?= $form->field($condition, 'lang_id')
-                ->dropDownList($langList, [
-                    'prompt' => 'Language',
-                    'onchange' => 'this.form.submit()'
-                ])->label(false); ?>
+                <?= $form->field($condition, 'lang_id')
+                    ->dropDownList($langList, [
+                        'prompt' => 'Language',
+                        'onchange' => 'this.form.submit()'
+                    ])->label(false); ?>
 
-            <?= $form->field($condition, 'service_id', [
-                'horizontalCssClasses' => [
-                    'wrapper' => false,
-                    'offset' => false,
-                    'label' => 'col-sm-12',
-                ],
-                'options' => [
-                    'class' => 'filter-checkbox-list form-group'
-                ]
-            ])
-                ->checkboxList($serviceList, [
-                    'itemOptions' => ['onchange' => 'this.form.submit()']
+                <?= $form->field($condition, 'service_id', [
+                    'horizontalCssClasses' => [
+                        'wrapper' => false,
+                        'offset' => false,
+                        'label' => 'col-sm-12',
+                    ],
+                    'options' => [
+                        'class' => 'filter-checkbox-list form-group'
+                    ]
                 ])
-                ->label('Service'); ?>
+                    ->checkboxList($serviceList, [
+                        'itemOptions' => ['onchange' => 'this.form.submit()']
+                    ])
+                    ->label('Service'); ?>
 
-            <?php ActiveForm::end(); ?>
-        </div>
+                <?php ActiveForm::end(); ?>
+            </div>
 
-        <?= Html::a('Clear all filters', ['/trainer/index'], ['class' => 'clear-filter']) ?>
+            <?= Html::a('Clear all filters', ['/trainer/index'], ['class' => 'clear-filter']) ?>
         </div>
     </aside>
 
@@ -171,62 +175,58 @@ $langList = Language::find()->select('lang_name')->where(['in', 'id', $langListS
 
             <?php foreach ($trainers as $trainer) {
 
-               echo "<li class='item trainer-item'>";
+                echo "<li class='item trainer-item'>";
 
                 if ($trainer->languages) {
                     echo '<ul class="lang-flag-list">';
 
                     foreach ($trainer->languages as $language) {
-                        echo '<li class="lang-flag"><img src="/img/flags/' . $language->lang_flag . '.svg" /></li>';
+                        $lang_flag = $language->lang_flag == '' ? $language->lang_code :
+                            $language->lang_flag;
+                        echo '<li class="lang-flag"><img src="/img/flags/' . $lang_flag . '.svg" /></li>';
                     }
 
                     echo '</ul>';
                 }
 
-                echo "<div class='item-thumbnail trainer-thumbnail'";
 
-                if ($trainer->thumb) {
-                    echo "style='background:url({$trainer->thumb}) no-repeat center center; background-size: cover'";
-                }
+                $style = $trainer->thumb ?
+                    "style='background:url({$trainer->thumb}) no-repeat center center; background-size: cover'" : null ?>
 
-                echo "></div>";
+                <div class='item-thumbnail trainer-thumbnail' <?= $style ?>></div>
 
+                <h2 class='name item-name trainer-name'>
+                    <?= Html::a($trainer->fullName, ['/trainer/profile', 'id' => $trainer->id]) ?>
+                </h2>
 
-                echo "<h2 class='name item-name trainer-name'>" . Html::a($trainer->name . ' ' . $trainer->surname, ['/trainer/profile', 'id' => $trainer->id]) . '</h2>';
+                <?php if ($trainer->services) { ?>
 
-                if ($trainer->services) {
+                    <ul class='service-list'>
 
-                    echo "<ul class='service-list'>";
+                        <?php $serviceNewList = '';
+                        foreach ($trainer->services as $service) {
+                            $serviceNewList .= '<li class="service">' . $service->service_name . '</li> ';
+                        } ?>
 
-                    $serviceNewList = '';
-                    foreach ($trainer->services as $service) {
-                        $serviceNewList .= '<li class="service">' . $service->service_name . '</li> ';
-                    }
+                        <?= $serviceNewList ?>
 
-                    echo $serviceNewList;
+                    </ul>
 
-                    echo '</ul>';
+                <?php }
 
-                }
+                if ($trainer->teachCountries) { ?>
+                    <ul class='teach-country-list'>
+                        <?php $profileCountryList = '';
+                        foreach ($trainer->teachCountries as $country) {
+                            $profileCountryList .= '<li class="teach-country-item">' . $country->country_name . '</li>,' . '&nbsp';
+                        }
 
-                if ($trainer->teachCountries) {
+                        echo substr($profileCountryList, 0, -6); ?>
 
-                    echo "<ul class='teach-country-list'>";
-
-                    $profileCountryList = '';
-                    foreach ($trainer->teachCountries as $country) {
-                        $profileCountryList .= '<li class="teach-country-item">' . $country->country_name . '</li>,' . '&nbsp';
-                    }
-
-                    echo substr($profileCountryList, 0, -6);
-
-                    echo '</ul>';
-                }
-
+                    </ul>
+                <?php }
                 echo '</li>';
-                }
-
-                ?>
+            } ?>
         </ul>
 
         <?= LinkPager::widget([
@@ -235,4 +235,3 @@ $langList = Language::find()->select('lang_name')->where(['in', 'id', $langListS
 
     </section>
 </div>
-
